a1e4b8dbf22cdadc61788cec3e90fbca68b6a6d5
Antonio Britto <antonio.britto.2000@hotmail.com>
2020-05-12 20:19:47 -0300

    Inserindo projeto no git hub

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './style.css'
 
-function App() {
+export default function App() {
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [result, setResult] = useState(0);
+
+  function increase(num1, num2) {
+    return -(-num1 - num2);
+  }
+  function decrease(num1, num2) {
+    return (num1 - num2);
+  }
+  function times(num1, num2) {
+    return (num1 * num2);
+  }
+  function division(num1, num2) {
+    return (num1 / num2);
+  }
+  function reset() {
+    setNumber1(0);
+    setNumber2(0);
+    setResult(0);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className='calculadora'>
+      <div className='orientacao'>
+        <h2>Digite os numeros nos espacos abaixo</h2>
+      </div>
+      <div className='valores'>
+        <input placeholder='Primero Número' value={number1} onChange={(e) => setNumber1(e.target.value)} />
+        <input placeholder='Segundo Número' value={number2} onChange={(e) => setNumber2(e.target.value)} />
+      </div>
+      <div clasname='operadores'>
+        <button className="soma" onClick={() => setResult(increase(number1, number2))}>+</button>
+        <button className="diferenca" onClick={() => setResult(decrease(number1, number2))}>-</button>
+        <button className='multiplicacao' onClick={() => setResult(times(number1, number2))}>*</button>
+        <button className='divisao' onClick={() => setResult(division(number1, number2))}>/</button>
+        <button className='reset' onClick={() => reset()}>reset</button>
+      </div>
+      <div className='result'>
+        <h3>Resultado:{result}</h3>
+      </div>
+    </div >
+  )
 }
-
-export default App;
